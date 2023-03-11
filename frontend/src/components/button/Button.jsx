9e8cd9231d340e8504cc45c706f879cbc3f4931a
Isaac Ndubuisi <theisaac3n@gmail.com>
2023-03-11 14:50:34 +0100

    navbar button

---
+++ frontend/src/components/button/Button.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Button = ({ button }) => {
+	return <div className="button">{button}</div>;
+};
+
+export default Button;
--- frontend/src/containers/navbar/Navbar.jsx
+++ frontend/src/containers/navbar/Navbar.jsx
@@ -1,8 +1,41 @@
 import React from "react";
 import "./navbar.css";
+import logo from "../../assets/logo.svg";
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<div className="budgetMe__navbar">
+			<div className="budgetMe__navbar-links">
+				<div className="budgetMe__navbar-links_logo">
+					<img src={logo} alt="" />
+				</div>
+				<div className="budgetMe__navbar-links_container">
+					<p>
+						<a href="#home">Home</a>
+					</p>
+					<p>
+						<a href="#moreInfo">What is BudgetMe</a>
+					</p>
+					<p>
+						<a href="#showcase">Features</a>
+					</p>
+					<p>
+						<a href="#articles">Articles</a>
+					</p>
+					<p>
+						<a href="#dashboard">My Dashboard</a>
+					</p>
+				</div>
+				<div className="budgetMe__navbar-sign">
+					<p>Sign In</p>
+					<button type="button">
+						<span>Sign Up</span>
+						<i />
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
