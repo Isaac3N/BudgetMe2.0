4d0fecafa8c9d0bcb44c57c88300a37cc4e06ce1
Isaac Ndubuisi <theisaac3n@gmail.com>
2023-03-15 18:05:33 +0100

    header without the assets, media query and animations, plus working get started button

--- frontend/src/containers/header/Header.jsx
+++ frontend/src/containers/header/Header.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import "./header.css";
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<div className="budgetMe__header section__padding" id="home">
+			<div className="budgetMe__header-content">
+				<p>Hey 👋 , start tracking you income and expenses with budgetMe 🇳🇬 </p>
+				<h1 className="gradient__text">
+					Gain Financial Freedom with BudgetMe.
+				</h1>
+				<h3>BudgetMe allows you to track your goals ...</h3>
+
+				<button type="button">
+					<span>Get Started</span>
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
