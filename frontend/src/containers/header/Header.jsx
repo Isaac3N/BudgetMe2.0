5abaca1f72c0da9abc2f5cc3c364b166c8b97bba
Isaac Ndubuisi <theisaac3n@gmail.com>
2023-03-21 08:34:34 +0100

    finished header with animated blob, not responsive or animated

--- frontend/src/containers/header/Header.jsx
+++ frontend/src/containers/header/Header.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./header.css";
+import image1 from "../../assets/header_image1.jpg";
+import image2 from "../../assets/header_image2.jpg";
+import blob from "../../assets/blob.svg";
 
 const Header = () => {
 	return (
@@ -7,13 +10,24 @@ const Header = () => {
 			<div className="budgetMe__header-content">
 				<p>Hey 👋 , start tracking you income and expenses with budgetMe 🇳🇬 </p>
 				<h1 className="gradient__text">
-					Gain Financial Freedom with BudgetMe.
+					Gain Financial Freedom, with BudgetMe.
 				</h1>
 				<h3>BudgetMe allows you to track your goals ...</h3>
 
 				<button type="button">
 					<span>Get Started</span>
 				</button>
+			</div>
+			<div className="budgetMe__header-img1">
+				<img src={image1} alt="image1" />
+			</div>
+
+			<div className="budgetMe__header-img2">
+				<img src={image2} alt="image2" />
+			</div>
+
+			<div className="budgetMe__header-blob float">
+				<img src={blob} alt="blob" />
 			</div>
 		</div>
 	);
