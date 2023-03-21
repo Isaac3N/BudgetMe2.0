add99361569b3e7ca8e654fd41c9955f3732fcd0
Isaac Ndubuisi <theisaac3n@gmail.com>
2023-03-21 09:31:07 +0100

    added typewriter effect

--- frontend/src/containers/header/Header.jsx
+++ frontend/src/containers/header/Header.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./header.css";
+
+import Typewriter from "typewriter-effect";
+
 import image1 from "../../assets/header_image1.jpg";
 import image2 from "../../assets/header_image2.jpg";
 import blob from "../../assets/blob.svg";
@@ -12,7 +15,28 @@ const Header = () => {
 				<h1 className="gradient__text">
 					Gain Financial Freedom, with BudgetMe.
 				</h1>
-				<h3>BudgetMe allows you to track your goals ...</h3>
+				<h3>
+					<Typewriter
+						options={{
+							autostart: true,
+							loop: true,
+						}}
+						onInit={(typewriter) => {
+							typewriter
+
+								.typeString("BudgetMe allows you to track your goals!")
+								.pauseFor(2000)
+								.deleteChars(5)
+								.typeString("expenses!")
+								.pauseFor(2000)
+								.deleteChars(8)
+								.typeString("income!")
+								.pauseFor(2000)
+								.deleteChars(6)
+								.start();
+						}}
+					/>
+				</h3>
 
 				<button type="button">
 					<span>Get Started</span>
