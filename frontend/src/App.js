0db1be4c3b111d99d00cee576e1cd8ff5e42c7d2
Isaac Ndubuisi <theisaac3n@gmail.com>
2023-03-24 06:53:29 +0100

    added locomotive scroll on app js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./App.css";
-
 import {
 	Articles,
 	Features,
@@ -12,20 +11,46 @@ import {
 	WhyBudget,
 } from "./containers/baseContainers";
 
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
+import "locomotive-scroll/dist/locomotive-scroll.css";
+
+import { AnimatePresence } from "framer-motion";
+
 const App = () => {
+	const containerRef = useRef(null);
+
 	return (
 		<div>
-			<div className="gradient__bg">
-				<Navbar />
-				<Header />
-			</div>
+			<LocomotiveScrollProvider
+				options={{
+					smooth: true,
+					// ... all available Locomotive Scroll instance options
+				}}
+				watch={
+					[
+						//..all the dependencies you want to watch to update the scroll.
+						//  Basicaly, you would want to watch page/location changes
+						//  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
+					]
+				}
+				containerRef={containerRef}
+			>
+				<AnimatePresence>
+					<main data-scroll-container ref={containerRef}>
+						<div className="gradient__bg">
+							<Navbar />
+							<Header />
+						</div>
 
-			<Features />
-			<Showcase />
-			<MoreInfo />
-			<WhyBudget />
-			<Articles />
-			<Footer />
+						<Features />
+						<Showcase />
+						<MoreInfo />
+						<WhyBudget />
+						<Articles />
+						<Footer />
+					</main>
+				</AnimatePresence>
+			</LocomotiveScrollProvider>
 		</div>
 	);
 };
